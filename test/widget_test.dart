a8b531fce60f9ab1ae4ff6f168597a47fde17248
dcarpencu <dcarpencu@gmail.com>
2022-04-25 14:39:05 +0300

    Working properly

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:movie_list_api_v2/src/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MoviesApp(store: null,));
+    await tester.pumpWidget(const MoviesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
